c4302573d4094715fbec9b3a58a48cd21c3d0bed
Kris <kriskuiper74@gmail.com>
2020-09-10 20:55:38 +0200

    Render error page on plants

--- api/plants.js
+++ api/plants.js
@@ -1,8 +1,15 @@
 const render = require('../utils/render')
 
 module.exports = async (request, response) => {
-  const plantsTemplate = await render('plants', {})
+  try {
+    const plantsTemplate = await render('plants', {})
 
-  response.status(200)
-  response.send(plantsTemplate)
+    response.status(200)
+    response.json(plantsTemplate)
+  } catch (error) {
+    const errorTemplate = await render('error', {})
+
+    response.status(500)
+    response.send(errorTemplate)
+  }
 }

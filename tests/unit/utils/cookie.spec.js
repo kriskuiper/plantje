26778eed173f5c240ed094c2aab86effe17c8eb0
Kris <kriskuiper74@gmail.com>
2020-09-12 13:30:28 +0200

    Make cookie test more robust

--- tests/unit/utils/cookie.spec.js
+++ tests/unit/utils/cookie.spec.js
@@ -1,8 +1,9 @@
 const cookie = require('@/utils/cookie')
 const cookieTestKey = 'test'
 const cookieTestValue = '1a2b3c4d'
+const cookieTestTwoValue = '1a2b3c4e'
 const clearedCookieValue = 'test=deleted; HttpOnly; SameSite=Lax path=/.netlify/functions; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-const multipleCookies = 'test=1a2b3c4d; HttpOnly; SameSite=Lax path=/.netlify/functions; expires=Sun, 12 Sep 2021 11:05:09 GMT,test=1a2b3c4d; HttpOnly; SameSite=Lax path=/.netlify/functions; expires=Sun, 12 Sep 2021 11:05:09 GMT'
+const multipleCookies = 'test=1a2b3c4d; HttpOnly; SameSite=Lax path=/.netlify/functions; expires=Sun, 12 Sep 2021 11:05:09 GMT,test_two=1a2b3c4e; HttpOnly; SameSite=Lax path=/.netlify/functions; expires=Sun, 12 Sep 2021 11:05:09 GMT'
 
 describe('Cookie library test', () => {
   it('Can give a created cookie string', () => {
@@ -21,8 +22,10 @@ describe('Cookie library test', () => {
 
   it('Can get cookie value from multiple cookies', () => {
     const testValue = cookie.get(multipleCookies, 'test')
+    const secondTestValue = cookie.get(multipleCookies, 'test_two')
 
     expect(testValue).toEqual(cookieTestValue)
+    expect(secondTestValue).toEqual(cookieTestTwoValue)
   })
 })
 

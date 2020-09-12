c208dc7caffb582c7442c4357899dcb42244b763
Kris <kriskuiper74@gmail.com>
2020-09-12 15:31:38 +0200

    Create add to plants function (untested)

--- functions/add-to-plants.js
+++ functions/add-to-plants.js
@@ -1,8 +1,40 @@
-exports.handler = () => {
-  return {
-    statusCode: 301,
-    headers: {
-      Location: '/plants'
-    },
+const { addPlant, addUser } = require('../db/services/user')
+const { cookies } = require('../utils/constants')
+const cookie = require('../utils/cookie')
+const errorPage = require('./views/error')
+const withDatabaseConnection = require('./middleware/with-database-connection')
+
+exports.handler = withDatabaseConnection(
+  async ({ headers, body }) => {
+    const userIdCookie = cookie.get(headers.cookie, cookies.USER_ID)
+
+    if (userIdCookie) {
+      return addPlant(userIdCookie, body.plant_id)
+        .then(() => ({
+          statusCode: 302,
+          headers: {
+            Location: '/plants'
+          }
+        }))
+        .catch(error => ({
+          statusCode: 500,
+          body: errorPage(error)
+        }))
+    }
+
+    const encryptedUserId = await addUser()
+    
+    return addPlant(encryptedUserId, body.plant_id)
+      .then(() => ({
+        statusCode: 302,
+        headers: {
+          Location: '/plants',
+          'Set-Cookie': cookie.set(cookies.USER_ID, encryptedUserId)
+        }
+      }))
+      .catch(() => ({
+        statusCode: 500,
+        body: errorPage(error)
+      }))
   }
-}
+)

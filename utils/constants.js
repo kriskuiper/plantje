f47f2bed4c721363878599fb24d3400e1d4617c9
Kris <kriskuiper74@gmail.com>
2020-09-12 15:31:10 +0200

    Add cookies to constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,12 @@
-exports.errors = {
+const errors = {
   FAILED_REQUEST: 'Failed to fetch data'
+}
+
+const cookies = {
+  USER_ID = 'plantly_user_id'
+}
+
+module.exports = {
+  errors,
+  cookies
 }

0fd0a2f5da03256f05a18c2c15e1e5527c01845a
Kris <kriskuiper74@gmail.com>
2020-09-12 16:27:44 +0200

    Give error page some love

--- functions/views/error.js
+++ functions/views/error.js
@@ -1,18 +1,36 @@
-module.exports = (error) => `
+module.exports = (errorMessage) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/styles.css">
-  <title>Something went fout</title>
+  <title>Plantly</title>
+  <style>
+    pre {
+      font-family: monospace;
+      padding: 0.75em;
+      color: #4af626;
+      background: black;
+      white-space: normal;
+      font-size: 1.225em;
+    }
+
+    pre > span {
+      color: orange;
+    }
+  </style>
 </head>
 <body>
   <header>
-    <h1>Kapot ¯\_(ツ)_/¯</h1>
+    <h1>Kapot</h1>
   </header>
 
-  <p>Dat ging niet zo goed, ga <a href="/">terug naar home</a></p>
+  <main>
+    <pre><span>Error:</span> ${errorMessage}</pre>
+    <p>Sorry man ¯\\_(ツ)_/¯</p>
+    <p>De oplossing is om <a href="/">terug te gaan naar home</a></p>
+  </main>
 
   <script src="/scripts/bundle.js"></script>
 </body>

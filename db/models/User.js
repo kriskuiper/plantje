189c59feb3a0a23e1eb74e4fa483dc3cdbecb49c
Kris <kriskuiper74@gmail.com>
2020-09-12 11:31:32 +0200

    Fix: get correct schema

--- db/models/User.js
+++ db/models/User.js
@@ -1,4 +1,4 @@
-const { plantSchema } = require('./Plant')
+const { plantSchema } = require('../schemas/plant')
 const { ObjectId, Schema, model } = require('mongoose')
 
 const userSchema = new Schema({

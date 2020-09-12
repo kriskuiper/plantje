52e0b29bc9884a8f909434ec69d173a45c3ef5b9
Kris <kriskuiper74@gmail.com>
2020-09-12 11:32:02 +0200

    Remove async since we don't do anything async

--- functions/plants.js
+++ functions/plants.js
@@ -1,22 +1,12 @@
 const plantsPage = require('./views/plants')
 const errorPage = require('./views/error')
 
-exports.handler = async () => {
-  try {
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'text/html'
-      },
-      body: plantsPage(),
-    }
-  } catch(error) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'text/html'
-      },
-      body: errorPage()
-    }
+exports.handler = ({ httpMethod }) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: plantsPage(),
   }
 }

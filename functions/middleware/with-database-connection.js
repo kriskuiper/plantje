f0e814b6ab154cb58845eb2bcf0b37d66c0499d9
Kris <kriskuiper74@gmail.com>
2020-09-12 15:44:32 +0200

    Add JSDocs to middleware

--- functions/middleware/with-database-connection.js
+++ functions/middleware/with-database-connection.js
@@ -1,6 +1,13 @@
 const connectToDB = require('../../db/services/connect')
 const errorPage = require('../views/error')
 
+/**
+ * @description Ensures a database connection is made before a
+ * serverless function is fired
+ * @param {function} handler
+ * @return {function} connectToDB function, if that succeeds it returns
+ * the handler. Otherwise it renders the error page with a database error
+ */
 module.exports = (handler) => {
   return (event, context, callback) => {
     return connectToDB()

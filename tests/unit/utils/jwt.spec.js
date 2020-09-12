8a5cbb00f8402b0df0b3556182cb1ec92084bd7e
Kris <kriskuiper74@gmail.com>
2020-09-12 13:21:37 +0200

    Let the JWT test breathe some more

--- tests/unit/utils/jwt.spec.js
+++ tests/unit/utils/jwt.spec.js
@@ -7,17 +7,20 @@ describe('JWT test', () => {
 
     expect(token).toMatch(jwtRegex)
   })
+
   it('Can decrypt text', () => {
     const token = encrypt('a-token')
     const decryptedValue = decrypt(token)
 
     expect(decryptedValue).toEqual('a-token')
   })
+
   it('Can encrypt objects', () => {
     const token = encrypt({ name: 'Test person' })
 
     expect(token).toMatch(jwtRegex)
   })
+  
   it('Can decrypt objects', () => {
     const token = encrypt({ name: 'Test person' })
     const decryptedValue = decrypt(token)

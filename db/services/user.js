c6b25bb8d7c5740d25c573232256f23b82ff4f8b
Kris <kriskuiper74@gmail.com>
2020-09-12 11:43:21 +0200

    Add JSdocs to db services

--- db/services/user.js
+++ db/services/user.js
@@ -1,17 +1,45 @@
 const User = require('../models/User')
 
-exports.removePlant = (encryptedUserId, plantId) => {
+/**
+ * @description Removes a plant from a users' document
+ * 
+ * @param {string} encryptedUserId The encrypted user id from a cookie
+ * 
+ * @param {string} plantId The id of the plant to remove
+ * 
+ * @return {Promise} A promise which resolves to the id of the plant that was removed
+ */
+exports.removePlant = async (encryptedUserId, plantId) => {
   // Remove a plant from a user
 }
 
-exports.addPlant = (encryptedUserId, plantId) => {
+/**
+ * @description Adds a plant to the users' document
+ * 
+ * @param {string} encryptedUserId The encrypted user id from a cookie
+ * 
+ * @param {string} plantId The id of the plant to remove
+ * 
+ * @return {Promise} A promise which resolves to the id of the plant that was added
+ */
+exports.addPlant = async (encryptedUserId, plantId) => {
   // Add a plant to a user
 }
 
-exports.addUser = () => {
-  // Add a user to the DB
+/**
+ * @description Adds a user to the database
+ * 
+ * @return {Promise} A promise which resolves to true or rejects to false
+ */
+exports.addUser = async () => {
+  // Add a user to the DB, returns the encrypted user id
 }
 
-exports.removeUser = () => {
+/**
+ * @description Removes a user from the database
+ * 
+ * @return {Promise} A promise which resolves to true or rejects to false
+ */
+exports.removeUser = async () => {
   // Remove a user from the DB
 }

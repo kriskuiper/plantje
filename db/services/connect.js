724d8281eafe7e8c4ac4db62862e29e1af92f548
Kris <kriskuiper74@gmail.com>
2020-09-12 15:30:44 +0200

    Create DB connect service

---
+++ db/services/connect.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const { MONGODB_URI } = process.env
+
+module.exports = () => {
+  return mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+}

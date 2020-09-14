1d097a131ed0015757824d82c5808c867d8ce3f1
Kris <kriskuiper74@gmail.com>
2020-09-14 21:12:39 +0200

    Just use empty array op user schema

--- db/schemas/user.js
+++ db/schemas/user.js
@@ -1,8 +1,6 @@
 const { Schema, ObjectId } = require('mongoose')
 
-const plantSchema = require('./plant')
-
 module.exports = new Schema({
   _id: ObjectId,
-  plants: [plantSchema]
+  plants: []
 })

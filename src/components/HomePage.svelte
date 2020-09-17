143e023ac24b3d4f0dff2e64249681231ce889e2
Kris <kriskuiper74@gmail.com>
2020-09-17 23:09:16 +0200

    Change name to Plantje

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -2,6 +2,6 @@
   import Link from './Link.svelte'
 </script>
 
-<h1>Welcome to Plantly!</h1>
+<h1>Welcome to Plantje!</h1>
 <p>This is now a simple Svelte app.</p>
 <Link href="/plants">To plants page</Link>

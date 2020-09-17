47126fa6417c21bdfdaa9c8a35dc3e843b070bb4
Kris <kriskuiper74@gmail.com>
2020-09-17 20:35:21 +0200

    Get plants data before showing to user

--- functions/plants.js
+++ functions/plants.js
@@ -1,11 +1,20 @@
-const plantsPage = require('./views/plants')
+const cookie = require('../utils/cookie')
+const { cookies } = require('../utils/constants')
+const { getPlants } = require('../db/services/user')
 
-exports.handler = ({ httpMethod }) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html'
-    },
-    body: plantsPage(),
-  }
-}
+const plantsPage = require('./views/plants')
+const withDatabaseConnection = require('./middleware/with-database-connection')
+
+exports.handler = withDatabaseConnection(
+  async ({ headers }) => {
+    const userIdCookie = cookie.get(headers.cookie, cookies.USER_ID)
+    const plants = await getPlants(userIdCookie)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html'
+      },
+      body: plantsPage(plants),
+    }
+})

ee665a14cc45f98a7f269ff4ae0079d5a301c8f8
Kris Kuiper <kkuiper@enrise.com>
2020-12-27 19:58:17 +0100

    feat: be able to save plants on a plants page

--- src/pages/PlantsPage.svelte
+++ src/pages/PlantsPage.svelte
@@ -1,6 +1,33 @@
 <script>
+  import { onMount } from 'svelte'
+
+  import { savedPlants } from '../store'
+
   import Link from '../components/Link.svelte'
+  import Plant from '../components/Plant.svelte'
+
+  let initialPlantsData = []
+
+  const getPlantsData = async (plantIds) => {
+    const body = JSON.stringify(plantIds)
+    const response = await fetch('/.netlify/functions/get-plants', {
+      method: 'POST',
+      body
+    })
+    const plantsData = await response.json()
+
+    return plantsData
+  }
+
+  onMount(async () => {
+    initialPlantsData = await getPlantsData($savedPlants)
+  })
 </script>
 
 <h1>Your plants will be dropped here</h1>
+
+{#each initialPlantsData as plant}
+  <Plant {plant} />
+{/each}
+
 <Link href="/">To home page</Link>

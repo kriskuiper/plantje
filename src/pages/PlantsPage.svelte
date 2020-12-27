154f9fe6f82172eded7ef821a7f5f92c8626f0f9
Kris Kuiper <kkuiper@enrise.com>
2020-12-27 21:34:03 +0100

    feat: show error if something goes wrong

--- src/pages/PlantsPage.svelte
+++ src/pages/PlantsPage.svelte
@@ -7,27 +7,48 @@
   import Plant from '../components/Plant.svelte'
 
   let initialPlantsData = []
+  let isLoading = false
+  let hasError = false
 
   const getPlantsData = async (plantIds) => {
-    const body = JSON.stringify(plantIds)
-    const response = await fetch('/.netlify/functions/get-plants', {
-      method: 'POST',
-      body
-    })
-    const plantsData = await response.json()
+    try {
+      const body = JSON.stringify(plantIds)
+      const response = await fetch('/.netlify/functions/get-plants', {
+        method: 'POST',
+        body
+      })
+      const plantsData = await response.json()
 
-    return plantsData
+      return plantsData
+    } catch (error) {
+      throw new Error(error)
+    }
   }
 
   onMount(async () => {
-    initialPlantsData = await getPlantsData($savedPlants)
+    try {
+      isLoading = true
+      initialPlantsData = await getPlantsData($savedPlants)
+    } catch {
+      hasError = true
+    } finally {
+      isLoading = false
+    }
   })
 </script>
 
 <h1>Your plants will be dropped here</h1>
 
-{#each initialPlantsData as plant}
-  <Plant {plant} />
-{/each}
+{#if isLoading}
+  <p>Loading plantdata...</p>
+{:else}
+  {#if hasError}
+    <p>Failed to load plants :(</p>
+  {:else}
+    {#each initialPlantsData as plant}
+    <Plant {plant} />
+    {/each}
+  {/if}
+{/if}
 
 <Link href="/">To home page</Link>
